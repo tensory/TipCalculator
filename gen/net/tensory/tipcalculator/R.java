8969c26d2f65cd551c201582a1785bd3514d1a0f
Ari Lacenski <alacenski@gmail.com>
2013-08-27 16:23:33 -0700

    Final updates after testing

--- gen/net/tensory/tipcalculator/R.java
+++ gen/net/tensory/tipcalculator/R.java
@@ -24,13 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080003;
-        public static final int button2=0x7f080004;
-        public static final int button3=0x7f080005;
-        public static final int editText1=0x7f080002;
-        public static final int textView1=0x7f080001;
-        public static final int textView2=0x7f080000;
+        public static final int action_settings=0x7f080008;
+        public static final int btn_10percent=0x7f080003;
+        public static final int btn_15percent=0x7f080001;
+        public static final int btn_20percent=0x7f080002;
+        public static final int et_bill_amount=0x7f080000;
+        public static final int tipAmountSummary=0x7f080004;
+        public static final int tv_amount=0x7f080006;
+        public static final int tv_currency_symbol=0x7f080005;
+        public static final int tv_label=0x7f080007;
     }
     public static final class layout {
         public static final int activity_tip_calculator=0x7f030000;
@@ -42,6 +44,13 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int hint_enter_total=0x7f050007;
+        public static final int label_10percent=0x7f050003;
+        public static final int label_15percent=0x7f050004;
+        public static final int label_20percent=0x7f050005;
+        public static final int txt_currency_symbol=0x7f050009;
+        public static final int txt_label_amount=0x7f050006;
+        public static final int txt_tip_amount=0x7f050008;
     }
     public static final class style {
         /** 
